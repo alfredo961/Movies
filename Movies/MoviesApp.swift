7e716939d4f4489319aa36bbfe8ac5a05861ed56
Alfredo <magmart96@gmail.com>
2024-02-03 14:37:27 -0600

    Initial Commit

---
+++ Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by Alfredo González on 3/2/24.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
